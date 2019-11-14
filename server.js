79e75beff99a77466d0cef43e8d05ca6a1a4469f
Luiz Henrique <luizhenriqque15@gmail.com>
2019-11-14 08:27:44 -0400

    :rocket: Configuração ambiente

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const path = require('path');
 const nomeApp = process.env.npm_package_name;
 const app = express();
 
-app.use(express.static(`$/dist/$`));
+// Serve only the static files form the angularapp directory
+app.use(express.static(__dirname + '/dist/AngularStarWars'));
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(`$/dist/$/index.html`));
+app.get('/', function(req,res) {
+
+  res.sendFile(path.join(__dirname+'/dist/AngularStarWars/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
