d5c540e6bb1ba2ce3f89db64ea2570f83807ab59
Luiz Henrique <luizhenriqque15@gmail.com>
2020-01-07 10:44:27 -0400

    Teste do CleanWebpackPlugin

---
+++ config/helpers.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const path = require('path');
+
+const _root = path.resolve(__dirname, '..');
+
+function root(args) {
+  args = Array.prototype.slice.call(arguments, 0);
+  console.log(path.join.apply(path, [_root].concat(args)));
+  return path.join.apply(path, [_root].concat(args));
+}
+
+exports.root = root;
---
+++ src/vendor.ts
@@ -0,0 +1,13 @@
+// Angular
+import '@angular/animations';
+import '@angular/common';
+import '@angular/core';
+import '@angular/forms';
+import "@angular/router";
+import "@angular/cdk";
+import "@angular/material";
+
+//RX
+import 'rxjs';
+import 'rxjs-compat';
+
